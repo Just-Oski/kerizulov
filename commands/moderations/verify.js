6baa2a0270caabe6af17896887e7caf719c2519e
Kacper C. || Oski#5377 <74201331+Just-Oski@users.noreply.github.com>
2022-06-04 07:51:07 +0200

    Update verify.js

--- commands/moderations/verify.js
+++ commands/moderations/verify.js
@@ -17,14 +17,26 @@ module.exports = {
 
 			if (alreadyHasRole) return message.channel.send('Ale ten ziom jest zweryfikowany tego typu').then((m) => m.delete({ timeout: 5000 }));
 
-			const embed = new MessageEmbed()
-				.setTitle(`Weryfikacja ${member}`)
-				.setDescription(`${message.author} pomyślnie zweryfikowano osobnika: ${member.user}`)
-				.setColor('f3f3f3')
-				.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-				.setFooter(new Date().toLocaleString());
+			var d = new Date,
+			dformat = [d.getMonth()+1,
+			d.getDate(),
+			d.getFullYear()].join('/')+' '+
+			[d.getHours()+2,
+			d.getMinutes(),
+			d.getSeconds()].join(':');
 
-			return member.roles.add(roleName).then(() => message.channel.send(embed));
+			const embed = new MessageEmbed()
+				.setTitle(`Weryfikacja ${member.username}`)
+				.setDescription(`${message.author} pomyślnie zweryfikował: ${member.user}`)
+				.setColor('6cf542')
+				.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+				.setFooter(d);
+
+			return member.roles.add(roleName).then(() => 
+			message.channel.send(embed)),
+			message.channel.edit({ name: `zweryfikowany-${member.username}` })
+			.then(console.log)
+			.catch(console.error);;
 		} catch (e) {
 			return message.channel.send('Spróbuj ponownie').then((m) => m.delete({ timeout: 5000 }));
 		}
